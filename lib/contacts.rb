51ef8b3804b136f584a341e4622c67b503513cb7
Justine Bricknell <justinegayton@gmail.com>
2018-06-28 21:20:49 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,8 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, data|
-binding.pry
+    binding.pry
     #data.each do |attribute, value|
   #  if attribute == :favorite_ice_cream_flavors["strawberry"]
+end
 end
